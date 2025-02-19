935817ecd456015cb91fd64e2701eeae84147b79
jesccy <jaspreet3480@gmail.com>
2025-02-19 17:33:21 +0530

    commit changes

--- Arrays/findIndex-method/387-first-unique-character-in-a-string.js
+++ Arrays/findIndex-method/387-first-unique-character-in-a-string.js
@@ -7,7 +7,7 @@ var firstUniqChar = function(s) {
     return index !== -1 ? index : -1;  // Return -1 if no unique character exists
 };
 //test cases
-let s = "leetcode";
+let s = "leetcode";  
 // Output: 0
 console.log(firstUniqChar(s));
 console.log(firstUniqChar("loveleetcode"));
